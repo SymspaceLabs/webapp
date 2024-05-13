4d4a77f6bf14c3ad37f631184505d2811704efd0
Thilak A/L Sundaram <thilak014@gmail.com>
2024-05-13 23:43:50 +0800

    Added login

--- src/pages-sections/sessions/page-view/login.jsx
+++ src/pages-sections/sessions/page-view/login.jsx
@@ -28,6 +28,43 @@ const LoginPageView = ({
     password: yup.string().required("Password is required"),
     email: yup.string().email("invalid email").required("Email is required")
   });
+
+  const onSubmit = async (values) => {
+    console.log(values)
+
+    const body ={
+      email: values.email,
+      password: values.password,
+    }
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/auth/login`,
+        {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+  
+      if (response.ok) {
+        // API call succeeded
+        const data = await response.json();
+        console.log(data);
+        alert('Login successful!');
+        // closeDialog?.();
+
+      } else {
+        // API call failed
+        const errorData = await response.json();
+        console.error('Login failed:', errorData);
+        alert('Login failed. Please try again.');
+      }
+    } catch (error) {
+      console.error('Error during Login:', error);
+      alert('An error occurred. Please try again later.');
+    }
+  };
   const {
     values,
     errors,
@@ -38,13 +75,10 @@ const LoginPageView = ({
   } = useFormik({
     initialValues,
     validationSchema,
-    onSubmit: values => {
-      console.log(values);
-      closeDialog?.();
-    }
+    onSubmit
   });
   return <form onSubmit={handleSubmit}>
-      <BazaarTextField mb={1.5} fullWidth name="email" size="small" type="email" variant="outlined" onBlur={handleBlur} value={values.email} onChange={handleChange} label="Email or Phone Number" placeholder="exmple@mail.com" error={!!touched.email && !!errors.email} helperText={touched.email && errors.email} />
+      <BazaarTextField mb={1.5} fullWidth name="email" size="small" type="email" variant="outlined" onBlur={handleBlur} value={values.email} onChange={handleChange} label="Email" placeholder="exmple@mail.com" error={!!touched.email && !!errors.email} helperText={touched.email && errors.email} />
 
       <BazaarTextField mb={2} fullWidth size="small" name="password" label="Password" autoComplete="on" variant="outlined" onBlur={handleBlur} onChange={handleChange} value={values.password} placeholder="*********" type={visiblePassword ? "text" : "password"} error={!!touched.password && !!errors.password} helperText={touched.password && errors.password} InputProps={{
       endAdornment: <EyeToggleButton show={visiblePassword} click={togglePasswordVisible} />
