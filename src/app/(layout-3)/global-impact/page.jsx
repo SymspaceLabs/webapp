b01cc6f02b10ad6ecefd5a6be6fa6b6095a0dbd7
Thilak A/L Sundaram <thilak014@gmail.com>
2024-09-09 20:13:10 +0800

    Updated blank pages for every navbar menu

---
+++ src/app/(layout-3)/global-impact/page.jsx
@@ -0,0 +1,20 @@
+import { Box } from "@mui/material";
+
+export const metadata = {
+  title: "Global Impact",
+  description: `Bazaar is a React Next.js E-commerce template. Build SEO friendly Online store, delivery app and Multi vendor store`,
+  authors: [{
+    name: "UI-LIB",
+    url: "https://ui-lib.com"
+  }],
+  keywords: ["e-commerce", "e-commerce template", "next.js", "react"]
+};
+export default function GadgetShopTwo() {
+  return (
+    <div className="bg-white">
+      <Box sx={{ p:5, minHeight:'250px'}}>
+
+      </Box>
+    </div>
+  );
+}
